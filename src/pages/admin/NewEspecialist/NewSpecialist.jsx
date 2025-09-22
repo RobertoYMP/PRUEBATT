a9a10f67a948e1ae147c434c2e5c2ac04fb44517
AlisonCg <alis04cortesg@gmail.com>
2025-09-21 23:13:47 -0600

    Registrar nuevo médico especialista
    Agregar celdas faltantes del formulario de registro de nuevo médico especialista.
    
    Alicia Cortes

---
+++ src/pages/admin/NewEspecialist/NewSpecialist.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import FormField from '../../../components/FormField.jsx'
+import './NewSpecialist.css'
+
+export default function NewSpecialist(){
+  return (
+    <div className='newspecialist-container'>
+      <h2>Registrar nuevo médico especialista</h2>
+      <div className="card-newspecialist">
+        <div className="stack-2">
+          <div style={{flex:1}}><FormField label="Nombre:" /></div>
+          <div style={{flex:1}}><FormField label="Apellido:" /></div>
+        </div>
+        <div className="stack-2 field-padding-top">
+          <div style={{flex:1}}><FormField label="Fecha de nacimiento:" /></div>
+          <div style={{flex:1}} className="field">
+            <label>Sexo:</label>
+            <select>
+              <option>Mujer</option>
+              <option>Hombre</option>
+              <option>Otro</option>
+            </select>
+          </div>
+        </div>
+        <div className='stack-2 field-padding-top'>
+          <div style={{flex:1}}><FormField label="Cédula Profesional:" /></div>
+          <div style={{flex:1}}><FormField label="Teléfono de contacto profesional:" /></div>          
+        </div>
+        <div className="stack-2 field-padding-top">
+          <FormField label="Especialidad:" tyle={{flex:1}}/>
+          <div style={{flex:1}}><FormField label="Correo electrónico:" /></div>
+        </div>
+        <div className="stack-2 field-padding-top">
+          <div style={{flex:1}}><FormField label="Contraseña:" type="password" /></div>
+          <div style={{flex:1}}><FormField label="Confirmar contraseña:" type="password" /></div>
+        </div>
+        <div className="stack-2 field-padding-top">
+          <FormField className='photo-input' label="Foto de perfil (seleccionar archivo jpg o png):" style={{flex:1, backgroundColor:'var(--color-secundary)', color:'#fff', padding:'5px'}} type='file' accept="image/png, image/jpeg"/>
+        </div>
+      </div>
+      <hr/>
+      <div className="button-newspecialist-container add-margin-top">
+        <button className="button-secondary">Regresar</button>
+        <button className="button-primary">Guardar médico</button>
+      </div>
+    </div>
+  )
+}
