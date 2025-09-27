a3790813930c829690326d992e74629a33154459
AlisonCg <alis04cortesg@gmail.com>
2025-09-27 13:09:34 -0600

    Cambiar diseño de recuperar contraseña
    Cambiar css del formulario de cambiar contraseña.
    
    Alicia Cortés

--- src/pages/auth/ForgotPassword.jsx
+++ src/pages/auth/ForgotPassword.jsx
@@ -1,26 +1,49 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import FormField from '../../components/FormField.jsx'
+import '../../styles/ForgotPassword.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLock } from "@fortawesome/free-solid-svg-icons";
+import Button from '../../components/Button/Button.jsx'
 
 export default function ForgotPassword() {
   const [email, setEmail] = useState('')
   const [sent, setSent] = useState(false)
   function onSubmit(e){ e.preventDefault(); setSent(true) }
   return (
-    <div className="card stack" style={{maxWidth: 520, margin: '40px auto'}}>
-      <h2>Recuperar Contraseña</h2>
-      {!sent ? (
-        <form onSubmit={onSubmit} className="stack">
-          <FormField label="Ingrese su correo electrónico" type="email" value={email} onChange={e=>setEmail(e.target.value)} required />
-          <button className="btn">Enviar correo de recuperación</button>
-        </form>
-      ) : (
-        <div className="stack">
-          <div className="badge estable">Correo enviado</div>
-          <p>Te hemos enviado un enlace para restablecer tu contraseña. Revisa tu bandeja o spam.</p>
-          <Link to="/login" className="btn secondary">Iniciar Sesión</Link>
+    <div className='forgot-container'>
+      <div className='forgot-logo'>
+        <div className='forgot-container'>
+          <FontAwesomeIcon icon={faLock} />
         </div>
-      )}
+      </div>
+      <div className="forgot-card">
+        <div className='forgot-header'>
+          <div className='forgot-tittle'>
+            <h2>Recuperar Contraseña</h2>
+          </div>
+        </div>
+        <div className='body-forgot-card'>
+          {!sent ? (
+            <form onSubmit={onSubmit} className="stack">
+              <FormField label="Ingrese su correo electrónico a continuación:" type="email" value={email} onChange={e=>setEmail(e.target.value)} required />
+              <button className="button-primary forgot-button">Enviar correo de recuperación</button>
+            </form>
+          ) : (
+            <div className="mail-sent">
+              <div className="badge estable" style={{width: "80%"}}>Correo enviado</div>
+              <p>Te hemos enviado un enlace para restablecer tu contraseña. Revisa tu bandeja o spam.</p>
+              <Button
+                as={Link}
+                to="/login"
+                typeButton={"button-primary"}
+                content={"Iniciar sesión"}
+                borderRadius={"var(--default-radius)"}
+              />
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   )
 }
