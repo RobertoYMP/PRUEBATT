097d41a1822c634ee2d10e9345ab8b11df582a5f
D4ncg2510 <108159079+D4ncg2510@users.noreply.github.com>
2025-11-02 01:04:23 -0600

    Update Login.jsx

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -2,7 +2,6 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import FormField from '../../components/FormField.jsx'
-// ❌ elimina: import { login } from '../../mock/api.js'
 import { signIn, getRoleFromClaims } from '../../auth/cognito'
 import '../../styles/login.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
