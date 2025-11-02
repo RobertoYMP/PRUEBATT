2d63d22360fd7cc841af6bf6ef500f0f25420ddf
RobertoYMP <145694295+RobertoYMP@users.noreply.github.com>
2025-11-02 14:56:06 -0600

    Set global variables for Buffer and process
    Added global variables for Buffer and process to support compatibility.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
-import './styles/theme.css'
-  import './styles/typography.css'
-import './styles/global.css'
+import { Buffer } from 'buffer';
+window.global  = window;
+window.Buffer  = window.Buffer || Buffer;
+window.process = window.process || { env: {} };
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.jsx';
+import './styles/theme.css';
+import './styles/typography.css';
+import './styles/global.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -12,4 +17,4 @@ createRoot(document.getElementById('root')).render(
       <App />
     </BrowserRouter>
   </React.StrictMode>
-)
+);
