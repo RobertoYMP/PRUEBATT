4a06e24dec0cce48ab5a5ae039b9786a19f82a3b
Alicia Cortes <alis04cortesg@gmail.com>
2025-11-30 13:35:22 -0600

    Quitar imagen de especialistas.
    Alicia Cortés

--- src/pages/specialists/Specialists.jsx
+++ src/pages/specialists/Specialists.jsx
@@ -1,9 +1,6 @@
 import { useState } from 'react'
 import './Specialists.css'
 import '../../components/Popup/Popup'
-//import { Popup } from '../../components/Popup/Popup'
-//import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
-//import { Link } from 'react-router-dom';
 
 export default function Specialists(){
   const [showPopup, setShowPopup] = useState(true);
@@ -15,37 +12,11 @@ export default function Specialists(){
 
   return (
     <>
-      {/*
-      <Popup
-        icon={faTrashCan}
-        type={"info"}
-        tittle={"Generando tu prediagnóstico"}
-        message={"unsaved_patient_changes"}
-        showButton={false}
-        buttonProps={{
-          as: Link,
-          to: "/login",
-          typeButton: "button-primary",
-          content: "Iniciar Sesión",
-          borderRadius: "var(--default-radius)"
-        }}
-        onClose={() => setShowPopup(false)}
-        isVisible={showPopup}
-        extraButton={false}
-        extraButtonProps={{
-          as: Link,
-          to: "/login",
-          typeButton: "button-primary",
-          content: "Ingresar otro correo electrónico",
-          borderRadius: "var(--default-radius)"
-        }}
-      />*/}
       <div className='specialists-container'>
         <h2>Nuestros especialistas</h2>
         <div className='specialists-cards-container'>
           {items.map((x,i)=> (
             <div key={i} className="specialist-card">
-              <img src='./Logo/blueLogo.png'></img>
               <strong>{x.nombre}</strong>
               <div>{x.especialidad}</div>
               <div>{x.tel}</div>
